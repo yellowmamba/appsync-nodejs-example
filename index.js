c8431b53f31a346ba327a63008075fcd3625e517
Michael Wu <michael.wu@versent.com.au>
2019-05-31 15:55:50 +1000

    Yay!

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const fetch = require('node-fetch')
+const AWS = require('aws-sdk')
+const URL = require('url')
+const aws4 = require('aws4')
+const awscred = require('awscred')
+const util = require('util')
+
 exports.handler = async (event, context) => {
     console.log(event)
     return `First: Todo ID is: ${event.data.todoId}`
@@ -12,5 +19,49 @@ exports.handler2 = async (event, context) => {
 exports.postCognitoAuth = async (event, context) => {
   console.log('This is triggered by Cognito PostAuthentication')
   console.log('Event payload', event)
+  console.log('Env vars', process.env)
+
+  const loader = util.promisify(awscred.load)
+  const data = await loader()
+
+  const addTodo = `mutation addPrivateTodo($id: ID!, $name: String) {
+    addTodo(id: $id, name: $name) {
+      id,
+      name
+    }
+  }`
+
+  const details = {
+    id: `${Date.now()}`,
+    name: 'my private todo'
+  }
+
+  const postBody = {
+    query: addTodo,
+    operationName: 'addPrivateTodo',
+    variables: details
+  }
+
+  const uri = URL.parse(process.env.GRAPHQL_API_ENDPOINT)
+
+  const options = {
+    host: uri.host,
+    path: uri.path,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    region: "ap-southeast-2",
+    service: "appsync",
+    body: JSON.stringify(postBody)
+  }
+
+  console.log(`Posting...`, JSON.stringify(postBody, null, 2))
+
+  const result = await fetch(uri.href, aws4.sign(options, data.credentials))
+  console.log(result)
+  
+  console.log('After fetch operation is done!')
+
   return event
 }
